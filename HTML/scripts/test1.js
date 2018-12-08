6ecaa2757f8e59f9cb9315ad87a1014cddd0b7ea
ThingsMinder <647906635@qq.com>
2018-12-08 22:13:00 +0800

    update a small program

---
+++ HTML/scripts/test1.js
@@ -0,0 +1,14 @@
+'use strict'
+
+function dis_prompt(){
+    var name = prompt("Please input your name", 'Bill Gates');
+    if(name!=null && name!=''){
+        document.write('Hello, ' + name + ', How are you?' );
+        document.write('<br>')
+        document.write('<br>')
+        document.write('<br>')
+        document.write('<br>')
+        document.write('<br>')
+        document.write('I Love you!')
+    }
+}
