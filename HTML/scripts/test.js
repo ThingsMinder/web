d0c3f3cac887dbdb811bb87f01f133abe6c62b14
ThingsMinder <647906635@qq.com>
2018-12-08 17:43:32 +0800

    update HTML

---
+++ HTML/scripts/test.js
@@ -0,0 +1,16 @@
+'use strict'
+
+/*
+alert("Hello");
+document.write("<h1>Hello</h1>");
+function message(){
+    alert("Hello")
+}
+{
+document.write('<h1>这是标题1</h1>');
+document.write('<p>这是一个段落</p>');
+document.write('<p>这是另一个段落</p>');
+}
+*/
+
+
