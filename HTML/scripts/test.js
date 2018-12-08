80cb79a4eccd4b1e1788a262beab10e3226ebf94
ThingsMinder <647906635@qq.com>
2018-12-08 18:02:30 +0800

    update test.js

--- HTML/scripts/test.js
+++ HTML/scripts/test.js
@@ -11,7 +11,7 @@ document.write('<h1>这是标题1</h1>');
 document.write('<p>这是一个段落</p>');
 document.write('<p>这是另一个段落</p>');
 }
-*/
+
 
 var firstname = 'John';
 document.write(firstname);
@@ -19,4 +19,15 @@ document.write('<br>')
 firstname = 'Lee';
 document.write(firstname);
 
+*/
 
+var d = new Date();
+var time = d.getHours();
+
+if(time < 10){
+    document.write('<b>Good morning</b>')
+}else if(time>=10 && time<20){
+    document.write('<b>Good afternoon</b>')
+}else{
+    document.write("<b>Hello</b>")
+}
