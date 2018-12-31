e82663a50eea45fc6b33891fab504e44ca352431
ThingsMinder <647906635@qq.com>
2018-12-31 21:06:13 +0800

    update 18123103

--- WX/test1/pages/index/index.js
+++ WX/test1/pages/index/index.js
@@ -2,6 +2,11 @@
 
 Page({
   data:{
-    message:"Hello,World!"
+    message:'工号',
+    message1:"Felix",
+    message2:'Grace',
+    message3:'Shmily',
+    message4:'emilie',
+    message5:'ViVi',
   }
 })
