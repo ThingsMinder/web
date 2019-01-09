0f88ff84dcc14747576335b91aa958d6ec569f7e
ThingsMinder <647906635@qq.com>
2019-01-09 21:58:04 +0800

    update 19010902

--- WX/test1/pages/index/index.js
+++ WX/test1/pages/index/index.js
@@ -1,5 +1,6 @@
 //index.js
 
+/*
 Page({
 
   canvasIdErrorCallback(e){
@@ -24,9 +25,116 @@ Page({
     context.moveTo(125,80)
     context.arc(120,80,5,0,2 * Math.PI,true)
     context.stroke()
-    context.draw()
+    context.draw() 
+  }
+})
+
+*/
+
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    markers:[{
+      iconPath:'/resourses/others.png',
+      id:0,
+      latitude:23.099994,
+      longitude:113.324520,
+      width:50,
+      height:50
+    }],
+    polyline:[{
+      points:[{
+        longitude:113.3245211,
+        latitude:23.10229
+      },{
+        longitude:113.324520,
+        latitude:23.21229
+      }],
+      color:"#FF0000DD",
+      width:2,
+      dottedLine:true
+    }],
+    controls:[{
+      id:1,
+      iconPath:'/resourses/location.png',
+      position:{
+        left:0,
+        top:300 - 50,
+        width:50,
+        height:50
+      },
+      clickable:true
+    }]
+  },
+
+  regionchange(e){
+    console.log(e.type)
+  },
+
+  markertap(e){
+    console.log(e.markerId)
+  },
+
+  controltap(e){
+    console.log(e.controlId)
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+    
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+    
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+    
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
     
   }
-
-
 })
