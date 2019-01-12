34495d696bf9dc30bfa097248c0c2d2d9f304af2
ThingsMinder <647906635@qq.com>
2019-01-13 00:12:33 +0800

    update 19011202

--- JavaScript/HTML5/script/test_object.js
+++ JavaScript/HTML5/script/test_object.js
@@ -1,121 +1,71 @@
 'use strict'
 
-/*
-var now = new Date();
-document.write(now + '<br>');
-document.write(now.getFullYear() + '<br>');
-document.write(now.getMonth() + '<br>');
-document.write(now.getHours() + '<br>');
-document.write(now.getDate() + '<br>');
-document.write(now.getDay() + '<br>');
-document.write(now.getTime() + '<br>');
-
-
-if(now.getMonth() === 1 && now.getDate() === 14){
-    if(0 <= now.getHours() <= 24){
-        alert('Dear');
+var arr_for = ['f0','f1','f2','f3','f4','f5','f6','f7','f8','f9','f10'];
+var arr_past = [];
+var arr_for1,arr_for2;
+function choujiang1(){  
+    document.getElementById('btn_num_1').hidden = true;
+    var num = Math.floor(Math.random()*10)
+    document.getElementById('num_1').innerHTML = "一等奖获得者：" + '<br>' + arr_for[num];
+    var arr_past1 = arr_past.push(arr_for[num]);
+    if(num != 0){
+        //arr_for1 = arr_for.slice(0,num_del1).concat(arr_for.slice(num_del1+1));
+        arr_for.splice(num,1);
+        arr_for1 = arr_for;
     }else{
-        alert('日期已过');
+        arr_for.shift();
+        arr_for1 = arr_for;
     }
-}else{
-    alert('不是当月');
+    return num,arr_for1,arr_past1;
 }
 
+function number1(){
+    document.getElementById('num_1_1').innerHTML = "一等奖名单:" + "<br>" + arr_past + "<br>" 
+                                                 + "暂未获奖名单:" + "<br>" + arr_for1; 
+                                                 
+}
 
-var xiaoming = {
-    name:'小明',
-    age:15,
-    gender:true,
-    height:1.65,
-    grade:null,
-    skills:['JS','Java','Python','H5'],
-    toJSON:function(){
-        return {
-            'name':this.name,
-            'Age':this.age
-        };
+var arr_past2 = [];
+function choujiang2(){  
+
+
+    document.getElementById('line').innerHTML = '--------------------------------------------------------';
+    document.getElementById('btn_num_2').hidden = true;
+    var num1,num2;
+    num1 = Math.floor(Math.random()*10);
+    num2 = Math.floor(Math.random()*10);
+    while(num1 === num2){
+        num2 = Math.floor(Math.random()*10);
+     };
+    document.getElementById('num_2').innerHTML = "二等奖获得者：" + '<br>' + arr_for1[num1] + "&" + arr_for1[num2];
+    arr_past2.push(arr_for1[num1],arr_for1[num2]);
+
+    if(num1 < num2){
+        if(num1 !== 0 && num2 !==1){
+            arr_for2 = arr_for1.slice(0,num1).concat(arr_for1.slice(num1+1,num2)).concat(arr_for1.slice(num2+1));
+        }else if(num1 === 0 && num2 !== 1){
+            arr_for2 = arr_for1.slice(1,num2).concat(arr_for1.slice(num2+1));        
+        }else if(num1 === 0 && num2 === 1){
+            arr_for2 = arr_for1.slice(2);            
+        }    
+    }else{
+        if(num2 !== 0 && num1 !==1){
+            arr_for2 = arr_for1.slice(0,num2).concat(arr_for1.slice(num2+1,num1)).concat(arr_for1.slice(num1+1));
+
+        }else if(num2 === 0 && num1 !== 1){
+            arr_for2 = arr_for1.slice(1,num1).concat(arr_for1.slice(num1+1));           
+        }else if(num2 === 0 && num1 === 1){
+            arr_for2 = arr_for1.slice(2);             
+        }  
     }
-};
-
-function convert(key,value){
-    if(typeof value === 'string'){
-        return value.toUpperCase();
-    }
-    return value;
-}
-*/
-
-/*
-//alert(JSON.stringify(xiaoming,convert,' '));
-alert(JSON.stringify(xiaoming));
-*/
-
-/*
-var Student = {
-    name:'Robot',
-    height:1.2,
-    run:function(){
-        return this.name + ' is running...';
-    }
-
-};
-
-function createStudent(name){
-    var s = Object.create(Student);
-    s.name = name;
-    return s;
+    return arr_for1,arr_for2,arr_past2;
 }
 
-var xiaoming = createStudent('小明');
-alert(xiaoming.name);
-alert(xiaoming.run());
-document.write(xiaoming.run());
-alert(xiaoming.__proto__ === Student);
-*/
+function number2(){
+    document.getElementById('num_2_2').innerHTML = "一等奖名单：" + "<br>" + arr_past + "<br>" 
+                                                 + "二等奖名单：" + "<br>" + arr_past2 + "<br>"
+                                                 + "暂未获奖名单:" + "<br>" + arr_for2; ;
 
-/*
-function Student(props){
-    this.name = props.name || 'Unnamed';
+
 }
-
-Student.prototype.hello = function(){
-    alert('Hello, ' + this.name + '!');
-}
-
-function PrimaryStudent(props){
-    Student.call(this,props);
-    this.grade = props.grade || 1;
-}
-
-inherits(PrimaryStudent,Student);
-
-PrimaryStudent.prototype.getGrade = function(){
-    return this.grade;
-}
-*/
-
-var name = 'The Window';
-var object = {
-    name:'my object',
-    getNameFunc:function(){
-        return function(){
-            return this.name;
-        };
-    }
-};
-
-/*
-var object1 = {
-    name:'my object',
-    getNameFunc1:function(){
-        var that = this;
-        return function(){
-            return that.name;
-        };
-    }
-};
-*/
-
-alert(object.getNameFunc()());
-//alert(object1.getNameFunc1()());
 
