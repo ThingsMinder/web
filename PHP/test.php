88a20620b3673b8ea1ed0bb0e848091ada11e4ac
ThingsMinder <647906635@qq.com>
2019-02-20 22:44:34 +0800

    add Sites & update tets.php

--- PHP/test.php
+++ PHP/test.php
@@ -4,7 +4,8 @@
 <h1>My Page</h1>
 
 <?php
-echo "Hello World!\n";
+/*
+echo "Hello World!<br>";
 
 class Car{
     var $color;
@@ -32,6 +33,92 @@ $con = mysql_connect("localhost","root","11111111");
 if(!$con){
     die('Could not connect:'.mysql_error());
 }
+*/
+
+/*
+$x=5;
+$y=6;
+$z=$x+$y;
+$zz="$x+$y=";
+echo $zz;
+echo $z;
+*/
+
+/*
+//$x=5;
+//$y=10;
+function myTest(){
+    static $x;
+    static $y;
+    static $z;
+    echo "<P>在函数内部测试变量</p>";
+    echo "变量 x 是：$x <br>";
+    echo "变量 y 是：$y <br>";
+    echo "变量 z 是：$z <br>";
+
+    //$GLOBALS['y']=$GLOBALS['x']+$GLOBALS['y'];
+    //$GLOBALS['x']++;
+    $x++;
+    $y++;
+    $z++;    
+}
+myTest();
+myTest();
+myTest();
+myTest();
+
+echo"<p>在函数外测试变量：</p>";
+echo"A    "," B     ","C    ","D <br>";
+echo "变量 x 是：$x <br>";
+echo "变量 y 是：$y";
+*/
+
+/*
+$x=5985;
+var_dump($x);
+echo"<br>";
+$x=-345;
+var_dump($x);
+echo"<br>";
+$x=0x8c;
+var_dump($x);
+echo("<br>");
+$x=046;
+var_dump($x);
+echo"<br>";
+
+$x=1.35;
+var_dump($x);
+echo"<br>";
+
+$x=2e3;
+var_dump($x);
+echo"<br>";
+
+$x=2.5e3;
+var_dump($x);
+echo"<br>";
+
+$x=8e-5;
+var_dump($x);
+echo"<br>";
+
+$cars=array("VOLVO","BMW","SAAB");
+var_dump($cars);
+echo'<br>';
+
+echo strlen('Hello,World!');
+echo'<br>';
+
+echo str_word_count("Hello W orld!");
+echo"<br>";
+
+echo str_replace("World","Kitty","Hello,World!");
+*/
+
+
+
+
 
 
 ?>
