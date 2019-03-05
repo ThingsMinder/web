28f679e8dbc7b5481b66e51d96425c86005adebf
ThingsMinder <647906635@qq.com>
2019-03-05 18:14:46 +0800

    update 19030501

---
+++ PHP/test_ajax/test3/livesearch.js
@@ -0,0 +1,44 @@
+var xmlHttp;
+
+function showResult(str){
+    if(str.length==0){
+        document.getElementById('livesearch').innerHTML = '';
+        document.getElementById('livesearch').style.border = '0px';
+        return;
+    }
+
+    xmlHttp = GetXmlHttpObject();
+
+    if(xmlHttp==null){
+        alert('Browser does not support HTTP Request');
+        return;
+    }
+
+    var url='livesearch.php';
+    url = url + '?q=' + str;
+    url = url + '&sid=' + Math.random();
+    xmlHttp.onreadystatechange = stateChanged;
+    xmlHttp.open('GET',url,true);
+    xmlHttp.send(null);
+}
+
+function stateChanged(){
+    if(xmlHttp.readyState == 4 || xmlHttp.readyState == 'complete'){
+        document.getElementById('livesearch').innerHTML = xmlHttp.responseText;
+        document.getElementById('livesearch').style.border = '1px solid #A5ACB2';
+    }
+}
+
+function GetXmlHttpObject(){
+    var xmlHttp = null;
+    try{
+        xmlHttp = new XMLHttpRequest();
+    }catch(e){
+        try{
+            xmlHttp = new ActiveXObject('Msxml2.XMLHTTP');
+        }catch(e){
+            xmlHttp = new ActiveXObject('Microsoft.XMLHTTP');
+        }
+    }
+    return xmlHttp;
+}
