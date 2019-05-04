6faa873ab89635152a214b9dd36de4c689c9b035
ThingsMinder <647906635@qq.com>
2019-05-04 20:51:06 +0800

    update 190504

---
+++ PHP/test_ajax/test4/clienthint.js
@@ -0,0 +1,41 @@
+//'use strict';
+
+var xmlHttp;
+function showHint(str){
+    if(str.length == 0){
+        document.getElementById('txtHint').innerHTML="";
+        return;
+    }
+    xmlHttp = GetXmlHttpObject();
+    if(xmlHttp==null){
+        alert('Browser does not support HTTP Request');
+        return;
+    }
+    var url = "gethint.php";
+    url = url + "?q=" + str;
+    url = url + "&sid=" + Math.random();
+    //alert(url);
+    xmlHttp.onreadystatechange = stateChanged;
+    xmlHttp.open("GET",url,true);
+    xmlHttp.send(null);
+}
+
+function stateChanged(){
+    if(xmlHttp.readyState==4||xmlHttp.readyState=='complete'){
+        document.getElementById('txtHint').innerHTML = xmlHttp.responseText;
+    }
+}
+
+function GetXmlHttpObject(){
+    var xmlHttp = null;
+    try{
+        xmlHttp = new XMLHttpRequest();
+    }catch(e){
+        try{
+            xmlHttp = new ActiveXObject('Msxml2.XMLHTTP');
+        }catch(e){
+            xmlHttp = new ActiveXObject('Microsoft.XMLHTTP')
+        }
+    }
+    return xmlHttp;
+}
