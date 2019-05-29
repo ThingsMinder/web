57d49be31012e7298fe9065cc6df0eb6bea70e70
ThingsMinder <647906635@qq.com>
2019-05-29 15:01:14 +0800

    update 19052901

--- PHP/test_Min/test.php
+++ PHP/test_Min/test.php
@@ -9,6 +9,8 @@
     }
     */
 
+
+    /*
     function open_folder($folder){
         if(file_exists($folder)){
             opendir($folder);
@@ -25,6 +27,22 @@
         echo "error code: ".$ex->getCode().'<br>';
         echo "file dir: ".$ex->getFile().'<br>';
         echo "error line: ".$ex->getLine().'<br>';
+    }
+    */
+    function create_connection(){
+        $link = mysqli_connect('localhost','root','11111111')
+        or die('无法建立数据链接： '.mysqli_connect_error());
+        mysqli_query($link,"SET NAMES utf8");
+        return $link;
+        //echo "打开成功";
+    }
+
+
+    function execute_sql($link,$databases,$sql){
+        mysqli_select_db($link,$databases)
+            or die("打开数据库失败： ".mysqli_error($link));
+        $result = mysqli_query($link,$sql);
+        return $result;
     }
 
 ?>
