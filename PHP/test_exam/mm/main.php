241d4c9c8ed3e4df4dc1b84c230485359d275594
ThingsMinder <647906635@qq.com>
2019-07-09 14:33:15 +0800

    update 19070901

--- PHP/test_exam/mm/main.php
+++ PHP/test_exam/mm/main.php
@@ -3,15 +3,42 @@
     <head>
         <meta charset="utf-8">
         <title>main</title>
+        <script type="text/javascript">
+            function check_data(){
+                if(document.myForm.subject.value.length == 0){
+                    alert("主题字段不能为空!");
+                }else if(document.myForm.content.value.length == 0){
+                    alert("内容字段不能为空！");
+                }else{
+                    myForm.submit();
+                }
+            }
+        </script>
     </head>
     <body>
-        <p align='center'><img src=""></p>
-        <a href="modify.php">修改资料</a>
-        <a href="delete.php">删除资料</a>
-        <a href="logout.php">退出登录</a>
+        <p align='right'><img src="2.jpg" height="20" width="20">
+            <a href="modify.php">修改资料</a>
+            <a href="delete.php">删除资料</a>
+            <a href="logout.php">退出登录</a>    
+        </p>
+
+        <?php
+            require_once('dbtools.inc.php');
+
+            $records_per_page = 5;
+            
+
+
+
+        ?>
+
+
+
 
     </body>
 </html>
+
+
 
 
 
