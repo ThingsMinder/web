212be85e75d98b3254ea774b62ffa73ca744c83e
ThingsMinder <647906635@qq.com>
2020-02-06 20:58:14 +0800

    update 2020020601

--- PHP/test_2020/test01/test.php
+++ PHP/test_2020/test01/test.php
@@ -1,10 +1,38 @@
 <!DOCTYPE html>
 <html>
-    
+    <head>
+        <meta charset="utf-8">
+        <title>whatever</title>
+    </head>
     <body>
         <h1>whatever</h1>
         <?php
-            echo "Hello!";
+        /*
+            echo "Hello!"."<br>";
+            $a = 11;
+            echo ~$a;
+            echo "<br>";
+
+            $greet = function($name){
+                echo "Hello "."\r"."!"."\n".$name;
+            };
+
+            $greet('World');
+            $greet('PHP');
+
+            echo "<br>";
+
+            $path = $_SERVER['PHP_SELF'];
+            echo basename($path).'<br>';
+            echo basename($path,'.php').'<br>';
+        */
+
+            $URL = $_POST['mySelect'];
+            header("location:$URL");
+            exit();
+
+
+
         ?>
     </body>
 </html>
