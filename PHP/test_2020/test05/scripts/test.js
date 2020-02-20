4a91768538d6c5412fbfcbab2e58bf2affc1b768
ThingsMinder <647906635@qq.com>
2020-02-20 22:40:07 +0800

    update 20022001

---
+++ PHP/test_2020/test05/scripts/test.js
@@ -0,0 +1,28 @@
+
+//测试
+/*
+alert("Hello World!");
+alert(2>=3);
+alert(NaN === NaN);
+alert(isNaN(NaN));
+*/
+
+//alert(null == 0);
+
+alert("I \'m \"ok\"\!");
+alert("\x41");
+
+var arr = new Array(1,2,3,4);
+for(var i = 0; i <= arr.length; i ++){
+    
+    if(i != arr.length){
+        alert(arr[i]);
+    }else{
+        alert("it's over!");
+    }
+    
+    
+
+    //alert("i= " + i + ",arr" + "[" + i + "]= " + arr[i]);
+    
+}
