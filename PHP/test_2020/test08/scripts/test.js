7a07e35e09f6edb661361882db8cfd965b8d7f14
ThingsMinder <647906635@qq.com>
2020-03-23 23:23:40 +0800

    update 200323

--- PHP/test_2020/test08/scripts/test.js
+++ PHP/test_2020/test08/scripts/test.js
@@ -55,7 +55,7 @@ function count(){
 var results = count();
 var f1 = results[2];
 alert(f1());
-*/
+
 
 function create_counter(initial){
     var x = initial || 0;
@@ -68,4 +68,43 @@ function create_counter(initial){
 }
 
 var c1 = create_counter(10);
+
+
+
+function* fib(max){
+    var 
+        t,
+        a = 0,
+        b = 1,
+        n = 0;
+    while(n < max){
+        yield a;
+        [a,b] = [b,a + b];
+        n ++;
+    }
+    return;
+}
+
+for(var x of fib(10)){
+    //console.log(x);
+    document.write(x + '<br>');
+}
+*/
+
+//document.write(123..toString());
+
+/*
+var today = new Date();
+//alert(today.getDay());
+
+
+if(today.getMonth() === 2 && today.getDate() === 22){
+    alert('I love you!')
+}
+*/
+//alert('a,b ,x cd   c'.split(/[\s\,]+/));
+
+var re = /^(\d{3})-(\d{3,8})$/;
+
+alert(re.exec('010-12345667'));
 
